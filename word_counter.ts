00a7ec57cca64c59cb4b07ef8b1d1407a3ff7f82
CodeWithHassam <81418138+gell987@users.noreply.github.com>
2022-12-21 04:34:14 -0800

    Add files via upload

---
+++ word_counter.ts
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+import * as readline from "readline";
+import chalkAnimation from "chalk-animation";
+import inquirer from "inquirer";
+let check:boolean=false
+const sleep = (ms = 3000) => new Promise((r) => setTimeout(r, ms));
+async function greet() {
+  console.clear();
+  const rainboxTitle = chalkAnimation.rainbow(
+    "Hassam's word_counter PIAIC(PIAIC202061)\n"
+  );
+  await sleep();
+  rainboxTitle.stop();
+}
+
+async function main() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  function countWords(input: string): number {
+    return input.split(" ").length;
+  }
+  let input:any = await inquirer.prompt({
+    name:"input",
+    type:"input",
+    message:chalk.redBright("Enter some text:  ")
+  }) 
+    const wordCount = countWords(input.input);
+  
+    console.log(chalk.redBright(`There are ${wordCount} words in your input.`));
+  
+  
+  
+}
+
+async function main2() {
+  let q_or_not = await inquirer.prompt({
+    name:"q_or_not",
+    type:"input",
+    message:chalk.redBright(`
+    Enter q(to quit)
+    Enter counter(to use the word counter)
+    Enter your choice:  `)
+  })
+  if(q_or_not.q_or_not=="q"){
+    check = true
+  }
+  else if(q_or_not.q_or_not=="counter"){
+    await main()
+  }
+  
+}
+await greet()
+while(check==false){
+  await main2()
+}
